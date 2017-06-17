53d0a8667382d4ef97ca8dd644c5e785b06cd317
francoccopa <franco.ccopa@hotmail.com>
2017-06-17 04:00:51 -0500

    Modificación de atributos
    Modificación de atributos con las tablas de la nueva base de datos.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 Vue.use(VueResource);
 
-const BASE_URL = 'http://descubreperu.pe.hu/descubreperu/'
-const prov_url = 'http://descubreperu.pe.hu/descubreperu/lista_proveedores.php';
-const places_url = 'http://descubreperu.pe.hu/descubreperu/lista_lugares.php';
-const services_url = 'http://descubreperu.pe.hu/descubreperu/lista_servicios.php';
-const tours_url = 'http://descubreperu.pe.hu/descubreperu/lista_turistas.php';
-const paquetes_url = 'http://descubreperu.pe.hu/descubreperu/lista_paquetes.php';
+const BASE_URL = 'http://descubreperu.pe.hu/descubre-peru'
+const prov_url = 'http://descubreperu.pe.hu/descubre-peru/list_suppliers.php';
+const places_url = 'http://descubreperu.pe.hu/descubre-peru/list_places.php';
+const services_url = 'http://descubreperu.pe.hu/descubre-peru/list_services.php';
+const tours_url = 'http://descubreperu.pe.hu/descubre-peru/list_tourist.php';
+const paquetes_url = 'http://descubreperu.pe.hu/descubre-peru/list_package.php';
 
 const post_proveedor = BASE_URL + 'registrar_proveedor.php'
 const post_servicio = BASE_URL + 'registrar_servicio.php'
@@ -20,21 +20,26 @@ new Vue({
     paquetes: [],
     places: [],
     entityProv: {
-    	razon_social: '',
+    	business_name: '',
     	ruc: '',
-    	prov_direccion: '',
-    	prov_ciudad: '',
-    	prov_pais: '',
-    	prov_telefono: '',
-    	prov_correo: '',
-    	representante_legal: '',
+    	address: '',
+    	city: '',
+    	country: '',
+    	phone: '',
+    	email: '',
+    	representative: '',
     },
     entityServ: {
-        serv_nombre: '',
-        serv_tipo: '',
-        serv_descripcion: '',
-        lugartur_id: '',
-        prov_id: '',
+        id:'',
+        title: '',
+        description: '',
+        url_images:'',
+        type_service_id: '',
+        place_id: '',
+        supplier_id: '',
+        active:'',
+        message:'',
+        help:'',
     },
   },
   mounted: mounted,
@@ -43,14 +48,14 @@ new Vue({
   		console.log(this.entityProv);
   		this.$http.post(post_proveedor, this.entityProv).then(response => {
     		console.log(response.body);
-    		this.entityProv.razon_social = ''
+    		this.entityProv.business_name = ''
     		this.entityProv.ruc = ''
-    		this.entityProv.prov_direccion = ''
-    		this.entityProv.prov_ciudad = ''
-    		this.entityProv.prov_telefono = ''
-    		this.entityProv.prov_pais = ''
-    		this.entityProv.representante_legal = ''
-    		this.entityProv.prov_correo = ''
+    		this.entityProv.address = ''
+    		this.entityProv.city = ''
+    		this.entityProv.phone = ''
+    		this.entityProv.country = ''
+    		this.entityProv.representative = ''
+    		this.entityProv.email = ''
 		}, response => {
 		    console.log(response);
 		});
@@ -60,11 +65,11 @@ new Vue({
         console.log(this.entityServ);
         this.$http.post(post_servicio, this.entityServ).then(response => {
             console.log(response.body);
-            this.entityServ.serv_nombre = ''
-            this.entityServ.serv_tipo = ''
-            this.entityServ.serv_descripcion = ''
-            this.entityServ.lugartur_id = ''
-            this.entityServ.prov_id = ''
+            this.entityServ.title = ''
+            this.entityServ.type_service_id = ''
+            this.entityServ.description = ''
+            this.entityServ.place_id = ''
+            this.entityServ.supplier_id = ''
         }, response => {
             console.log(response);
         });
@@ -87,7 +92,8 @@ function mounted () {
     });
 
     this.$http.get(services_url).then( response => {
-    	this.services = response.body;
+        this.services = response.body;
+        console.log(response.body);
     }).catch( error => {
     	console.log(error);
     })
